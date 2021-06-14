76763c8e6f4f87e546c0c42d5341991f7a7d2f81
alarm10086 <alarm10086@gmail.com>
2021-06-15 00:15:07 +0800

    p4

---
+++ s2/p4/p4_2_1/polynomials.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+def quad(a,b,c):
+    ''''返回a*x**2 + b*x + c = 0形式的方程的解'''''
+    x1 = (-b + sqrt(b**2 - 4*a*c))/(2*a)
+    x2 = (-b - sqrt(b**2 - 4*a*c))/(2*a)
+    return x1,x2
+
+print(quad(2,7,-15))
